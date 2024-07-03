3b223018010953038a60d163b9328077b16c7092
Philip~ <philip.krause88@yahoo.de>
2024-07-03 08:59:34 +0200

    fix bgLogo path error

--- indexRevamp.js
+++ indexRevamp.js
@@ -31,12 +31,10 @@ const CONFIG = {
 // /// just for debugging
 // setupDebugLogging(CONFIG.debugLogPath);
 
-
-
 // Set client versions. Note: Prsym version works differently and is parsed from logs
 const gethVer = "1.14.3";
 const rethVer = "1.0.0";
-const prysmVer = "5.1.3";
+const prysmVer = "5.0.3";
 
 // function showHelp() {
 //   console.log("Usage: node script.js [options]");
