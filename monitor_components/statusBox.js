cb399e738e5e083ee3ceff06b6ec604037a72fb3
sfaber34 <sfaber34@gmail.com>
2024-07-29 15:49:08 -0600

    Re-added alchemy client to get mainnet block number

--- monitor_components/statusBox.js
+++ monitor_components/statusBox.js
@@ -3,6 +3,12 @@ import { mainnet } from "viem/chains";
 import blessed from "blessed";
 import { debugToFile } from "../helpers.js";
 
+const client = createPublicClient({
+  name: "client",
+  chain: mainnet,
+  transport: http("https://eth-mainnet.g.alchemy.com/v2/demo"),
+});
+
 const localClient = createPublicClient({
   name: "localClient",
   chain: mainnet,
@@ -38,30 +44,30 @@ export async function updateStatusBox(statusBox, screen) {
       //     0
       //   )}\nHighest Block: ${highestBlock.toFixed(0)}`
       // );
+    } else {
+      const blockNumber = await localClient.getBlockNumber();
+      const latestBlock = await client.getBlockNumber();
+
+      if (blockNumber === latestBlock) {
+        statusBox.setContent(
+          `FOLLOWING CHAIN HEAD\nCurrent Block: ${blockNumber}`
+        );
       } else {
-        const blockNumber = await localClient.getBlockNumber();
-        const latestBlock = await client.getBlockNumber();
-  
-        if (blockNumber === latestBlock) {
-          statusBox.setContent(
-            `FOLLOWING CHAIN HEAD\nCurrent Block: ${blockNumber}`
-          );
-        } else {
-          statusBox.setContent(
-            `CURRENT BLOCK\nBlock: ${blockNumber}/${latestBlock}`
-          );
-        }
+        statusBox.setContent(
+          `CURRENT BLOCK\nBlock: ${blockNumber}/${latestBlock}`
+        );
       }
-  
-      screen.render();
-    } catch (error) {
-      console.error();
-      debugToFile(
-        `updateStatusBox() Failed to update sync progress gauge: ${error}`,
-        () => {}
-      );
     }
+
+    screen.render();
+  } catch (error) {
+    console.error();
+    debugToFile(
+      `updateStatusBox() Failed to update sync progress gauge: ${error}`,
+      () => {}
+    );
   }
+}
 
 export function createStatusBox(grid, screen) {
   const statusBox = grid.set(5, 7, 1, 2, blessed.box, {
