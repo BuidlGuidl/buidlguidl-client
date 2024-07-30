424a570cdac17178630231f2abac50b9bd2d19d0
sfaber34 <sfaber34@gmail.com>
2024-07-30 15:43:23 -0600

    Added more words to be highlighted in the client logs

--- monitor_components/helperFunctions.js
+++ monitor_components/helperFunctions.js
@@ -58,25 +58,23 @@ export function highlightWords(line) {
   // Define words which should be highlighted in exec and consensus logs
   const highlightRules = [
     { word: "INFO", style: "{bold}{green-fg}" },
-    { word: "number", style: "{bold}{green-fg}" },
-    { word: "root", style: "{bold}{green-fg}" },
-    { word: "elapsed", style: "{bold}{green-fg}" },
-    { word: "hash", style: "{bold}{green-fg}" },
-    { word: "epoch", style: "{bold}{green-fg}" },
-    { word: "slot", style: "{bold}{green-fg}" },
-    { word: "finalizedEpoch", style: "{bold}{green-fg}" },
-    { word: "finalizedRoot", style: "{bold}{green-fg}" },
-    { word: "attestations", style: "{bold}{green-fg}" },
-    { word: "payloadHash", style: "{bold}{green-fg}" },
-    { word: "kzgCommitmentCount", style: "{bold}{green-fg}" },
-    { word: "inboundTCP", style: "{bold}{green-fg}" },
-    { word: "outboundTCP", style: "{bold}{green-fg}" },
-    { word: "total", style: "{bold}{green-fg}" },
-    { word: "updated", style: "{bold}{yellow-fg}" },
     { word: "WARN", style: "{bold}{yellow-fg}" },
     { word: "ERROR", style: "{bold}{red-fg}" },
+    { word: "updated", style: "{bold}{yellow-fg}" },
     { word: "blockchain:", style: "{bold}{blue-fg}" },
+    { word: "backfill:", style: "{bold}{blue-fg}" },
+    { word: "execution:", style: "{bold}{blue-fg}" },
+    { word: "flags:", style: "{bold}{blue-fg}" },
+    { word: "filesystem:", style: "{bold}{blue-fg}" },
+    { word: "gateway:", style: "{bold}{blue-fg}" },
+    { word: "genesis:", style: "{bold}{blue-fg}" },
+    { word: "initial-sync:", style: "{bold}{blue-fg}" },
+    { word: "node:", style: "{bold}{blue-fg}" },
     { word: "p2p:", style: "{bold}{blue-fg}" },
+    { word: "rpc:", style: "{bold}{blue-fg}" },
+    { word: "state-gen:", style: "{bold}{blue-fg}" },
+    { word: "sync:", style: "{bold}{blue-fg}" },
+    { word: "Syncing:", style: "{bold}{blue-fg}" },
   ];
 
   // Apply styles to the words
@@ -84,6 +82,9 @@ export function highlightWords(line) {
     const regex = new RegExp(`(${rule.word})`, "g");
     line = line.replace(regex, `${rule.style}$1{/}`);
   });
+
+  // Highlight words followed by "="
+  line = line.replace(/\b(\w+)(?==)/g, "{bold}{green-fg}$1{/}");
 
   return line;
 }
