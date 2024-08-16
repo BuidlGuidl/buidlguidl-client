8b04cd05147a8c14df0783e00e90a20c565a0a8d
sfaber34 <sfaber34@gmail.com>
2024-08-16 10:17:52 -0600

    Added screen height debug logging

--- monitor_components/statusBox.js
+++ monitor_components/statusBox.js
@@ -17,6 +17,8 @@ export function createStatusBox(grid, screen) {
   const row = screen.height < layoutHeightThresh ? 1 : 5;
   const rowSpan = screen.height < layoutHeightThresh ? 2 : 1;
 
+  debugToFile(`screen.height: ${screen.height}`, () => {});
+
   const statusBox = grid.set(row, 7, rowSpan, 2, blessed.box, {
     label: `Status`,
     border: {
