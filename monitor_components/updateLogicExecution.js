3548fb8ce5298ca5a9e58dbdb36f9eb3461b0991
sfaber34 <sfaber34@gmail.com>
2024-12-02 13:53:08 -0700

    troubleshooting for rpcInfoBox conditionally show only if owner options is used

--- monitor_components/updateLogicExecution.js
+++ monitor_components/updateLogicExecution.js
@@ -556,7 +556,7 @@ export async function showHideRethWidgets(
       debugToFile(
         `Attempting to append rpcInfoBox. owner value: ${owner}, type: ${typeof owner}`
       );
-      if (!screen.children.includes(rpcInfoBox) && owner !== null) {
+      if (!screen.children.includes(rpcInfoBox) && owner) {
         screen.append(rpcInfoBox);
       }
     }
@@ -594,7 +594,7 @@ export async function showHideGethWidgets(
       debugToFile(
         `Attempting to append rpcInfoBox. owner value: ${owner}, type: ${typeof owner}`
       );
-      if (!screen.children.includes(rpcInfoBox) && owner !== null) {
+      if (!screen.children.includes(rpcInfoBox) && owner) {
         screen.append(rpcInfoBox);
       }
     }
