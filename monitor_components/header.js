2080e6df28f31dc33d86691ce6b956ab7e74fd13
sfaber34 <sfaber34@gmail.com>
2025-02-13 10:05:24 -0700

    Changed get points url

--- monitor_components/header.js
+++ monitor_components/header.js
@@ -33,7 +33,7 @@ export function createHeader(grid, screen, messageForHeader) {
   async function fetchPoints(owner) {
     try {
       const response = await axios.get(
-        `https://rpc.buidlguidl.com:48544/yourpoints?owner=${owner}`
+        `https://stage.rpc.buidlguidl.com:48546/yourpoints?owner=${owner}`
       );
       return response.data.points;
     } catch (error) {
