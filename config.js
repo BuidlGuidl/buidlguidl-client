218e1791febc90f18924cada1cb7355eca8d017d
sfaber34 <sfaber34@gmail.com>
2025-06-30 10:20:05 -0600

    config change for prod rpc

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Base URL for BuidlGuidl RPC endpoints
-// export default "pool.mainnet.rpc.buidlguidl.com";
-export default "stage.rpc.buidlguidl.com";
+export default "pool.mainnet.rpc.buidlguidl.com";
+// export default "stage.rpc.buidlguidl.com";
