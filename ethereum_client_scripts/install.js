4f44f43cdc0e9421f9f4b0598b803cf60f92f1bf
sfaber34 <sfaber34@gmail.com>
2026-03-06 15:50:54 -0700

    updated reth 1.11.1 and lighthouse 8.1.1

--- ethereum_client_scripts/install.js
+++ ethereum_client_scripts/install.js
@@ -6,8 +6,8 @@ import { installDir } from "../commandLineOptions.js";
 import { debugToFile } from "./../helpers.js";
 
 export const latestGethVer = "1.16.7";
-export const latestRethVer = "1.9.3";
-export const latestLighthouseVer = "8.0.1";
+export const latestRethVer = "1.11.1";
+export const latestLighthouseVer = "8.1.1";
 
 export function installMacLinuxClient(clientName, platform) {
   const arch = os.arch();
