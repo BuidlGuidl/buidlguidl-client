a03483940d4eda47d5877a8a73e5584dd8ffe75d
sfaber34 <sfaber34@gmail.com>
2026-08-13 15:32:14 -0600

    Updated lighthouse to v8.2.1

--- ethereum_client_scripts/install.js
+++ ethereum_client_scripts/install.js
@@ -7,7 +7,7 @@ import { debugToFile } from "./../helpers.js";
 
 export const latestGethVer = "1.17.4";
 export const latestRethVer = "2.5.0";
-export const latestLighthouseVer = "8.2.0";
+export const latestLighthouseVer = "8.2.1";
 
 export function installMacLinuxClient(clientName, platform) {
   const arch = os.arch();
