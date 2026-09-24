3938a4aa3aad1403d78a53b830259d50ef9b4784
sfaber34 <sfaber34@gmail.com>
2026-09-24 11:00:08 -0600

    Switched RPC url to staging box

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Base URL for BuidlGuidl RPC endpoints
-export const BASE_URL = "pool.mainnet.rpc.buidlguidl.com";
-// export const BASE_URL = "stage.rpc.buidlguidl.com";
+// export const BASE_URL = "pool.mainnet.rpc.buidlguidl.com";
+export const BASE_URL = "stage.rpc.buidlguidl.com";
 
 // Bread contract address
 export const BREAD_CONTRACT_ADDRESS =
